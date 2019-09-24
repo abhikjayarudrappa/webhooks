b74709b35c9c56b32f6603dbadcccef5ac094d73
abhikjayarudrappa <55375540+abhikjayarudrappa@users.noreply.github.com>
2019-09-24 11:35:22 +0530

    Update big3.c

--- big3.c
+++ big3.c
@@ -4,7 +4,7 @@ void big3()
 {
     int num1, num2, num3;
  
-    printf("Enter the values of num1, num2 and num3\n");
+    printf("\n Enter the values of num1, num2 and num3\n");
     scanf("%d %d %d", &num1, &num2, &num3);
     printf("num1 = %d\tnum2 = %d\tnum3 = %d\n", num1, num2, num3);
     if (num1 > num2)
